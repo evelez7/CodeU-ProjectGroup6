74dd405dd784c0b080863776d3e4785058284b4a
Antonio Carmona <adcarmona98@gmail.com>
2017-05-23 16:39:13 -0700

    Added getInfo function to View
    Added the function getInfo to the View class in codeu.chat.client.core

--- src/codeu/chat/client/core/View.java
+++ src/codeu/chat/client/core/View.java
@@ -22,6 +22,7 @@ import codeu.chat.common.ConversationHeader;
 import codeu.chat.common.ConversationPayload;
 import codeu.chat.common.Message;
 import codeu.chat.common.NetworkCode;
+import codeu.chat.common.ServerInfo;
 import codeu.chat.common.User;
 import codeu.chat.util.Logger;
 import codeu.chat.util.Serializers;
@@ -135,5 +136,26 @@ final class View implements BasicView {
     }
 
     return messages;
+  }
+  
+  
+  public ServerInfo getInfo() {
+	  
+	  try (final Connection connection = source.connect()) {
+		  
+		 Serializers.INTEGER.write(connection.out(), NetworkCode.SERVER_INFO_REQUEST);
+		 
+		 if (Serializers.INTEGER.read(connection.in()) == NetworkCode.SERVER_INFO_RESPONSE) {
+			 final Time startTime = Time.SERIALIZER.read(connection.in());
+			 return new ServerInfo(startTime);
+		 } else {
+			LOG.error("Response from server failed.");
+		 }
+	  } catch (Exception ex) {
+	      System.out.println("ERROR: Exception during call on server. Check log for details.");
+	      LOG.error(ex, "Exception during call on server.");
+	  }
+	  
+	  return null;
   }
 }
