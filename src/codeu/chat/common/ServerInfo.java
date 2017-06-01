e7cdd6f3342e7dc8a85235186e45adc620b74faf
EVelez79 <erickvelez7@gmail.com>
2017-05-31 17:03:11 -0700

    Removed unnecessary imports

--- src/codeu/chat/common/ServerInfo.java
+++ src/codeu/chat/common/ServerInfo.java
@@ -1,17 +1,7 @@
 package codeu.chat.common;
 
-import codeu.chat.common.BasicView;
-import codeu.chat.common.ConversationHeader;
-import codeu.chat.common.ConversationPayload;
-import codeu.chat.common.Message;
-import codeu.chat.common.NetworkCode;
-import codeu.chat.common.User;
 import codeu.chat.util.Logger;
-import codeu.chat.util.Serializers;
-import codeu.chat.util.Time;
 import codeu.chat.util.Uuid;
-import codeu.chat.util.connections.Connection;
-import codeu.chat.util.connections.ConnectionSource;
 
 public final class ServerInfo {
   private final static Logger.Log LOG = Logger.newLog(ServerInfo.class);
