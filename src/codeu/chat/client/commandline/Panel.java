637663b1265a1df3a65e217adc2088a0cabd84a4
juliachin123 <juliachin8@gmail.com>
2017-06-01 19:12:02 -0700

    Update Panel.java
     Changed how the command from the command line is parsed and passed to the command interface

--- src/codeu/chat/client/commandline/Panel.java
+++ src/codeu/chat/client/commandline/Panel.java
@@ -48,10 +48,10 @@ final class Panel {
   // will be returned. True will be return if a command is found. Whether or not
   // the command was successful is not returned.
   //
-  public boolean handleCommand(String commandName, Scanner line) {
+  public boolean handleCommand(String commandName, List<String> args) {
     final Command command = commands.get(commandName);
     if (command != null) {
-      command.invoke(line);
+      command.invoke(args);
     }
     return command != null;
   }
