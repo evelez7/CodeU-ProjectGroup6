6e446d007b15aa39be1bf49bde050c1628e2dd68
Julia Chin <juliachin8@gmail.com>
2017-06-06 09:15:53 -0700

    Changed test methods

--- test/codeu/chat/util/TokenizerTest.java
+++ test/codeu/chat/util/TokenizerTest.java
@@ -1,36 +1,41 @@
 package codeu.chat.util;
+import java.util.*;
 import codeu.chat.util.Tokenizer;
 import org.junit.Test;
 import java.io.IOException;
 import static org.junit.Assert.*;
 public final class TokenizerTest {
 	@Test
-	Scanner input = new Scanner(System.in);
 	public void testWithNoQuotes() throws IOException {
-		final Tokenizer tokenizer = new Tokenizer();
+		final Tokenizer tokenizer = new Tokenizer("hello world how are you");
 		while (tokenizer.hasNext()){
-			assertEquals(tokenizer.next(), input.next());
+			assertEquals(tokenizer.next(), tokenizer.next());
 		}
 	}
 	@Test
 	public void testWithQuotes() throws IOException {
 		final Tokenizer tokenizer = new Tokenizer("\"hello world\" \"how are you\"");
-		while (tokenizer.hasNext){
+		assertEquals(tokenizer.next(), "hello world");
+		assertEquals(tokenizer.next(), "how are you");
+		assertEquals(tokenizer.next(), null);
+		/*
+		while (tokenizer.hasNext()){
 			int first_quote = 0;
 			int first_quote_location = 0;
 			for (int i= 0; i < tokenizer.length(); i++){
-				if (tokenizer.get(i).equals("\"")){
+				if (tokenizer.substring(i, i+1).equals("\"")){
 					first_quote++;
 					first_quote_location = i;
 				}
-				if (tokenizer.get(i).equals(" ") && ((first_quote % 2) != 0){
-					assertEquals(tokenizer.next(), tokenizer.substring(first_quote_location, i));
-				}
-				else if (i = tokenizer.length-1){
+				if (i == tokenizer.length-1){
 					assertEquals(tokenizer.next(), tokenizer.substring(first_quote_location, i));
 				}	
+				else if (tokenizer.substring(i, i+1).equals(" ") && ((first_quote % 2) != 0)){
+					assertEquals(tokenizer.next(), tokenizer.substring(first_quote_location, i));
+				} 
 			}
 		}
+		*/
 	}
 
 }
