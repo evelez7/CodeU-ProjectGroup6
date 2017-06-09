aa8b39506dfe8da20c45e589d4a9eed276ba79d3
EVelez79 <erickvelez7@gmail.com>
2017-06-09 11:27:27 -0700

    Revert to use of Scanner in Panel.java

--- src/codeu/chat/client/commandline/Panel.java
+++ src/codeu/chat/client/commandline/Panel.java
@@ -27,7 +27,7 @@ import java.util.Scanner;
 final class Panel {
 
   public interface Command {
-    void invoke(List<String> args);
+    void invoke(Scanner line);
   }
 
   private final Map<String, Command> commands = new HashMap<>();
